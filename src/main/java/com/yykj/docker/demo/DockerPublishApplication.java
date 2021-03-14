d2da6f1fb21332ed11e96627fea436fc500e8588
kelvin <hanqi_kelvin@163.com>
2021-03-14 15:27:57 +0800

    Initial commit

---
+++ src/main/java/com/yykj/docker/demo/DockerPublishApplication.java
@@ -0,0 +1,13 @@
+package com.yykj.docker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerPublishApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerPublishApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yykj/docker/demo/DockerPublishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yykj.docker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerPublishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
